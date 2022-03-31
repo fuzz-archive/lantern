b57d9ff3e5db3b45a87470343c61d081a66bc24b
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-03-31 14:43:42 +1100

    Update client

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,11 +1,33 @@
-import { createTRPCClient } from '@trpc/client';
-import type { AppRouter } from '../../server/src/router/index';
-import './polyfill';
+import { createTRPCClient, HTTPHeaders } from '@trpc/client'
+import { httpLink } from '@trpc/client/links/httpLink'
+import { splitLink } from '@trpc/client/links/splitLink'
+import type { AppRouter } from '../../server/src/router'
+import './polyfill'
 
-const client = createTRPCClient<AppRouter>({
-  url: 'http://localhost:3000/trpc'
-});
+interface ClientOptions {
+  port?: number
+  prefix?: string
+  headers?: HTTPHeaders
+}
 
-(async () => {
-  console.log(await client.query('hello', { username: 'bob' }))
-})()
+export function createClient(host: string, options: ClientOptions = {}) {
+  const port = options.port ?? 80
+  const prefix = options.prefix ?? '/trpc'
+  const url = port ? `${host}:${port}${prefix}` : `${host}${prefix}`
+  
+  return createTRPCClient<AppRouter>({
+    url,
+    headers: options.headers,
+    links: [
+      splitLink({
+        condition(op) {
+          return op.context.skipBatch === true
+        },
+        true: httpLink({
+          url
+        }),
+        false: httpLink({ url })
+      })
+    ]
+  })
+}
