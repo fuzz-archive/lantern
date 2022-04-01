2023c82a4d66be2f3e2aaea036921ac31add78cb
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-04-01 12:06:26 +1100

    Add Hooks

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,33 @@ import { createContext } from './router/context'
 import { appRouter } from './router/index'
 
 const app = fastify()
+  .addHook('onRequest', (_, res, done) => {
+    void res.headers({
+      'Cache-Control': 'public, max-age=7776000',
+      'X-Powered-By': 'Fuzzy Wuzzy, ArtieFuzzz#8298'
+    })
+
+    done()
+  })
+  .setNotFoundHandler((req, res) => {
+    void res.code(404)
+
+    return res.send({
+      code: 404,
+      error: false,
+      message: `${req.method} ${req.url} Is not a valid Route`
+    })
+  })
+  .setErrorHandler(async (err, _, res) => {
+    await res.code(500)
+
+    return res.send({
+      code: err.code,
+      message: 'Something went wrong...',
+      error: true,
+      error_message: `[${err.name}:${err.code}]: ${err.message}`
+    })
+  })
 
 app.register(fp(fastifyTRPCPlugin), {
   prefix: '/trpc',
